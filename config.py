9083ac8b0f60bd0d497620799b4d892edb157eb7
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-06-23 23:54:55 +0300

    add new changes made

--- config.py
+++ config.py
@@ -1,27 +1,57 @@
 import os
 
+
 class Config:
+    
+    '''
+    General configuration parent class
+    '''
     SECRET_KEY = 'Robert11'
-    UPLOADED_PHOTOS_DEST ='app/static/images'
-    MAIL_SERVER = 'smtp.googlemail.com'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Robert11@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
+     #email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Robert11@localhost/pitches'
-
-
-class ProdConfig(Config):
-    pass
-
-class DevConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Robert11@localhost/pitches'
-
+    SUBJECT_PREFIX = 'pitches'
+    SENDER_EMAIL = 'robertokello443@gmail.com'
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
     
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Robert11@localhost/pitches'
+    
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Robert11@localhost/pitches'
     DEBUG = True
-
+    ENV = 'development'
+    
 config_options = {
-    'development':DevConfig,
-    'production':ProdConfig
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
 }
---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='Robert11'
+export MAIL_USERNAME='Robertokello443@gmail.com'
+export MAIL_PASSWORD='37544329'
+
+python3.8 manage.py server
